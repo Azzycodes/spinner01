04e1cd5752503e6956caddef9e1f7a145dda1acd
cd <azzamathar@hotmail.com>
2019-07-30 18:33:44 +0000

    linted

--- spinner02.js
+++ spinner02.js
@@ -1,7 +1,8 @@
-const order = function(char,number) {
+const order = function(char, increment) {
   setTimeout(() => {
-      process.stdout.write(`\r${char}   `)},number)    
-}
+    process.stdout.write(`\r${char}   `);
+  },increment);
+};
 const rotate = function() {
   let charArray = ["|","/","-","\\", "|", "/","-","\\"];
   let delay = 300;
